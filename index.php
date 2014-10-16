d5f27368b21e96c122f2dd0a7248c2cc5b1f04e8
Jack M <secretagent12388@gmail.com>
2014-10-15 22:36:53 -0400

    version: w33d

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Is It Worth The Risk? </title>
+        <link rel=stylesheet type="text/css" href="stylesheet.css">
+        <link href='http://fonts.googleapis.com/css?family=Pathway+Gothic+One' rel='stylesheet' type='text/css'>
+    </head>
+    <body>
+        <div>
+            <h1> IS IT WORTH THE RISK? </h1>
+        </div>
+        <div>
+            <form Method="POST" Action="dickshit.php">
+                <input class="text" type="text" name="input">
+                <input type="submit" value="Submit">
+            </form>
+        </div>
+    </body>
+</html>
